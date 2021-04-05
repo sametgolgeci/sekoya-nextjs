7bbd4f0b817bf93f51fba5a6b07ed0090c1f6934
Samet Gölgeci <samet.golgeci@gmail.com>
2021-04-05 03:07:59 +0300

    Update 2.0
    - Index sayfası tasarlandı

--- pages/index.js
+++ pages/index.js
@@ -8,46 +8,48 @@ export default function Home() {
         <title>Sekoya</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    {/*
-      <main className="navigation">
-        <div className="navigation-logo">
-          <Link href="/"><a>LOGO</a></Link>
+      <main>
+        <div className="col-md-3 col-sm-12 col-xs-12 sidebar">
+          <div className="sidebar-logo">
+            <img className="sidebar-logo-img" src="logo.png" alt="logo"/>
+          </div>
+          <div className="sidebar-items">
+            <ul>
+              <li className="sidebar-item">
+                <Link href="#">Hakkımızda</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="#">Nasıl Çalışıyoruz</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="#">Hizmetlerimiz</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="#">Uygulamalarımız</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="#">İletişim</Link>
+              </li>
+            </ul> 
+          </div>
+          <div className="sidebar-social">
+            <div className="sidebar-social-item">Ins</div>
+            <div className="sidebar-social-item">Lin</div>
+            <div className="sidebar-social-item">Beh</div>
+          </div>
         </div>
-        <div className="navigation-nav">
-          <ul class="nav">
-            <li class="nav-item">
-              <Link href="/hakkimizda"><a class="nav-link">Hakkımızda</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/nasil-calisiyoruz"><a class="nav-link">Nasıl Çalışıyoruz</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/hizmetlerimiz"><a class="nav-link">Hizmetlerimiz</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/uygulamalarimiz"><a class="nav-link">Uygulamalarımız</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="#"><a class="nav-link">İletişim</a></Link>
-            </li>
-             <li class="nav-item">
-              <Link href="#"><a class="nav-link">TR | EN</a></Link>
-            </li>
-          </ul>
+        <div className="col-md-9 col-sm-12 col-xs-12 index">
+          <div className="col-md-6 col-sm-12 col-xs-12 index-img">
+            <img src="unsplash-1.jpg" alt="anasayfa"/>
+          </div>
+          <div className="col-md-6 col-sm-12 col-xs-12 index-text">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+            <p> laborum nesciunt possimus eum atque</p>
+            <div className="index-button">
+              <button type="button"><a href="#">??????????</a></button>
+            </div>
+          </div>
         </div>
-      </main>
-      */}
-      <main className="index">
-        <div className="index-logo">
-          <Link href="/"><a>LOGO</a></Link>
-        </div>
-        <div className="index-detail">
-          <p>Açıklama</p>
-        </div>
-        <a href="/hakkimizda">
-        <div className="index-button buttonn">
-          <span>HELLO</span>
-        </div></a>
       </main>
     </div>
   )
