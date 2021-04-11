40b9b54377b0b51663a90a41259f28d04d0d0144
Samet Gölgeci <samet.golgeci@gmail.com>
2021-04-12 02:57:46 +0300

    Update 2.4
    - Hizmetlerimiz sayfası tasarlandı

--- pages/hizmetlerimiz.js
+++ pages/hizmetlerimiz.js
@@ -1,106 +1,70 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagramSquare,faLinkedin,faBehanceSquare } from '@fortawesome/free-brands-svg-icons'
+import { faBars,faStream } from '@fortawesome/free-solid-svg-icons'
 
-export default function Hizmetlerimiz() {
+export default function Home() {
   return (
     <div>
       <Head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <title>Sekoya | Hizmetlerimiz</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <div className="navigation">
-        <div className="navigation-logo">
-          <Link href="/"><a>LOGO</a></Link>
-        </div>
-        <div className="navigation-nav">
-          <ul class="nav">
-            <li class="nav-item">
-              <Link href="/hakkimizda"><a class="nav-link">Hakkımızda</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/nasil-calisiyoruz"><a class="nav-link">Nasıl Çalışıyoruz</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/hizmetlerimiz"><a class="nav-link">Hizmetlerimiz</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="/uygulamalarimiz"><a class="nav-link">Uygulamalarımız</a></Link>
-            </li>
-            <li class="nav-item">
-              <Link href="#"><a class="nav-link">İletişim</a></Link>
-            </li>
-             <li class="nav-item">
-              <Link href="#"><a class="nav-link">TR | EN</a></Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <h1>Hizmetlerimiz</h1>
-      <div className="container">
-        <div class="row justify-content-md-center">
-          <div class="col-md-5 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-            <p><strong>Mobilya</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>            
+      <main>
+        <div className="col-md-3 col-sm-12 col-xs-12 sidebar">
+          <div className="sidebar-logo">
+            <Link href="/"><a>
+              <img className="sidebar-logo-img" src="logo-2.png" alt="logo"/>
+            </a></Link>
           </div>
-          <div class="col-md-5 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-            <p><strong>Mimari Tasarım ve Uygulama</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+          <div className="sidebar-items">
+            <ul>
+              <li className="sidebar-item">
+                <Link href="/ekip">Ekibimiz</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="/nasil-calisiyoruz">Nasıl Çalışıyoruz</Link>
+              </li>
+              <li className="sidebar-item active">
+                <Link href="/hizmetlerimiz">Hizmetlerimiz</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="/uygulamalarimiz">Uygulamalarımız</Link>
+              </li>
+              <li className="sidebar-item">
+                <Link href="/iletisim">İletişim</Link>
+              </li>
+            </ul> 
+          </div>
+          <div className="sidebar-social">
+            <div className="sidebar-social-item"><a href="#"><FontAwesomeIcon className="social-item" icon={faInstagramSquare}/></a></div>
+            <div className="sidebar-social-item"><a href="#"><FontAwesomeIcon className="social-item" icon={faLinkedin}/></a></div>
+            <div className="sidebar-social-item"><a href="#"><FontAwesomeIcon className="social-item" icon={faBehanceSquare}/></a></div>
           </div>
         </div>
-        <div class="row justify-content-md-center">
-          <div class="col-md-5 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-            <p><strong>Aydınlatma</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>            
-          </div>
-          <div class="col-md-5 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-            <p><strong>Hediyelik Eşya ve Aksesuar</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-          </div>
-        </div>    
-      </div>
-      <h1>Hizmetlerimiz 2</h1>
-      <div className="container">
-        <div class="row justify-content-md-center hizmet">
-          <div class="col-md-4 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-          </div>
-          <div class="col-md-6 b">
-            <p><strong>Mimari Tasarım ve Uygulama</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-          </div>
-        </div>  
-        <div class="row justify-content-md-center hizmet">
-          <div class="col-md-6 b">
-            <p><strong>Mimari Tasarım ve Uygulama</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-          </div>
-          <div class="col-md-4 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
+        <div className="col-md-9 col-sm-12 col-xs-12 hizmet">
+          <div className="hizmet-texts">
+            <div className="hizmet-text text-1">
+              <h1>Mobilya</h1>
+              <p>DENEME 1</p>
+            </div>
+            <div className="hizmet-text text-2">
+              <h1>Mimari Tasarım ve Uygulama</h1>
+              <p>Açıklama, detay 1,2 satır olacak şekilde</p>
+            </div>
+            <div className="hizmet-text text-3">
+              <h1>Hediyelik Eşya ve Aksesuar</h1>
+              <p>DENEME 3</p>
+            </div>
+            <div className="hizmet-img"></div>
           </div>
         </div>
-        <div class="row justify-content-md-center hizmet">
-          <div class="col-md-4 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-          </div>
-          <div class="col-md-6 b">
-            <p><strong>Mimari Tasarım ve Uygulama</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-          </div>
-        </div>  
-        <div class="row justify-content-md-center hizmet">
-          <div class="col-md-6 b">
-            <p><strong>Mimari Tasarım ve Uygulama</strong></p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-          </div>
-          <div class="col-md-4 b">
-            <img src="background.jpg" class="hizmet-img" alt="..."/>
-          </div>
-        </div>
-      </div>
+      </main>
     </div>
-  )
+  )  
 }
+
