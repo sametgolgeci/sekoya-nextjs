62fa7386a1b6301bdf59e5ae133cac596ef41f06
Samet Gölgeci <samet.golgeci@gmail.com>
2021-06-18 19:33:28 +0300

    Update 3.5

--- pages/nasil-calisiyoruz.js
+++ pages/nasil-calisiyoruz.js
@@ -8,6 +8,7 @@ export default function Nasil() {
   return (
     <div>
       <Head>
+        <html lang="tr"/>
         <meta charset="utf-8"/>
         <meta http-equiv="x-ua-compatible" content="ie=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
@@ -17,10 +18,42 @@ export default function Nasil() {
       <main>
         <div className="col-md-sidebar col-sm-12 col-xs-12 sidebar">
           <div className="sidebar-logo">
-            <Link href="/"><a>
-              <img className="sidebar-logo-img sidebar-logo-1" src="sekoya-logo.png" alt="sekoya-logo"/>
-              <img className="sidebar-logo-img sidebar-logo-2" src="sekoya-logo-2.png" alt="sekoya-logo"/>
-            </a></Link>
+            <Link href="/"><a><img className="sidebar-logo-img sidebar-logo-1" src="sekoya-logo.png" alt="sekoya-logo"/></a></Link>
+          </div>
+          <div className="sidebar-mobil">
+            <div className="mobil-logo">
+              <Link href="/"><a><img className="mobil-logo-img" src="sekoya-logo-2.png" alt="sekoya-logo"/></a></Link>
+            </div>
+            <div className="mobil-nav">
+              <button><FontAwesomeIcon className="dropdown-item" icon={faBars}/></button>
+              <nav>
+                <ul>
+                  <li className="sidebar-item">
+                    <Link href="/hakkimizda">Hakkımızda</Link>
+                  </li>
+                  <li className="sidebar-item">
+                    <Link href="/ekip">Ekibimiz</Link>
+                  </li>
+                  <li className="sidebar-item active">
+                    <Link href="/nasil-calisiyoruz">Nasıl Çalışıyoruz</Link>
+                  </li>
+                  <li className="sidebar-item">
+                    <Link href="/hizmetlerimiz">Hizmetlerimiz</Link>
+                  </li>
+                  <li className="sidebar-item">
+                    <Link href="/projelerimiz">Projelerimiz</Link>
+                  </li>
+                  <li className="sidebar-item">
+                    <Link href="/iletisim">İletişim</Link>
+                  </li>
+                </ul>
+                <div className="sidebar-social-mobil">
+                  <div className="sidebar-social-item"><a href="https://www.linkedin.com/company/sekoyaahsap" target="_blank"><FontAwesomeIcon className="social-item" icon={faInstagram}/></a></div>
+                  <div className="sidebar-social-item"><a href="https://www.instagram.com/sekoyaahsap/" target="_blank"><FontAwesomeIcon className="social-item" icon={faLinkedinIn}/></a></div>
+                  <div className="sidebar-social-item"><a href="https://wa.me/905327076816" target="_blank"><FontAwesomeIcon className="social-item" icon={faWhatsapp}/></a></div>
+                </div>  
+              </nav>
+            </div>
           </div>
           <div className="sidebar-items">
             <ul>
